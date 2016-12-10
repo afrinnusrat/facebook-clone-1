7f15430874bd59f6299f84c60e01e6be73a26310
Benjamin Arias <benjamin.j.arias@gmail.com>
2016-12-10 17:12:12 +0100

    feat: Wrote Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  render() {
+    return (
+      <button 
+        style={this.props.style ? this.props.style : ''}
+        value={this.props.value}
+        onClick={this.props.onClick}
+      />
+    );
+  }
+}
+
+export default Button;
