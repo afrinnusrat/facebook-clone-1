638b112a435b3af892aafde26fc74ee8bb3db03f
Benjamin Arias <benjamin.j.arias@gmail.com>
2016-12-12 15:03:35 +0100

    feat: Finished writing Disclaimer components tests

--- src/components/Disclaimer/Disclaimer.spec.js
+++ src/components/Disclaimer/Disclaimer.spec.js
@@ -3,22 +3,44 @@ import { shallow } from 'enzyme';
 import { expect } from 'chai';
 
 import Disclaimer from './';
+import DisclaimerSection from '../DisclaimerSection';
 
 describe('Disclaimer', () => {
   let wrapper;
+  let disclaimerTest;
 
   beforeEach(() => {
-    const test = {
-      text: 'test',
-      section: {
-        text: 'test',
-        image: 'http://www4.ac-nancy-metz.fr/clg-p-e-victor-corcieux/new/wp-content/uploads/th1.jpg',
-      },
+    disclaimerTest = {
+      title: 'test',
+      sections: [
+        {
+          text: 'test',
+          image: 'http://www4.ac-nancy-metz.fr/clg-p-e-victor-corcieux/new/wp-content/uploads/th1.jpg',
+        },
+        {
+          text: 'test',
+          image: 'http://www4.ac-nancy-metz.fr/clg-p-e-victor-corcieux/new/wp-content/uploads/th1.jpg',
+        },
+        {
+          text: 'test',
+          image: 'http://www4.ac-nancy-metz.fr/clg-p-e-victor-corcieux/new/wp-content/uploads/th1.jpg',
+        },
+      ],
     };
-    wrapper = shallow(<Disclaimer disclaimer={test} />);
+    wrapper = shallow(<Disclaimer disclaimer={disclaimerTest} />);
   });
 
   it('should render an div with the disclaimer-container class', () => {
     expect(wrapper.find('div.disclaimer-container')).to.have.length(1);
+  });
+
+  it('should render 3 DisclaimerSection components', () => {
+    const wrapper = shallow(
+      <Disclaimer disclaimer={disclaimerTest}>
+        <DisclaimerSection />
+      </Disclaimer>
+    );
+
+    expect(wrapper.find(DisclaimerSection)).to.have.length(3);
   });
 });
--- src/components/Disclaimer/index.js
+++ src/components/Disclaimer/index.js
@@ -1,24 +1,24 @@
 import React, { Component, PropTypes } from 'react';
 
-// import DisclaimerSection from '../DisclaimerSection';
+import DisclaimerSection from '../DisclaimerSection';
 
 class Disclaimer extends Component {
-  /*
   renderDisclaimerSections() {
-    this.props.disclaimer.sections.map((section) => {
+    const sections = this.props.disclaimer.sections.map((section) => {
       return (
         <DisclaimerSection key={section.text} image={section.image} text={section.text} />
       );
     });
+    return <div>{sections}</div>;
   }
-  */
+
   render() {
     return (
       <div className="disclaimer-container">
         <div className="disclaimer-title">
           <p>{this.props.disclaimer.title}</p>
         </div>
-        {/* this.renderDisclaimerSections.bind(this)() */}
+        { this.renderDisclaimerSections.bind(this)() }
       </div>
     );
   }
