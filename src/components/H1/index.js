04b9ed3b0f489c6dddd1fd01c07ded7429359bbd
Benjamin Arias <benjamin.j.arias@gmail.com>
2016-12-15 09:01:03 +0100

    fix: Wrong prop name in H1 component

--- src/components/H1/index.js
+++ src/components/H1/index.js
@@ -3,13 +3,13 @@ import React, { Component, PropTypes } from 'react';
 class H1 extends Component {
   render() {
     return (
-      <h1>{this.props.text}</h1>
+      <h1>{this.props.title}</h1>
     );    
   }
 }
 
 H1.propTypes = {
-  text: PropTypes.string,
+  title: PropTypes.string,
 };
 
 export default H1;
